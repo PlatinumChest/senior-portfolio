ac15b984151946553d3c3f3dfff35248e82efd22
PlatinumChest <155034647+PlatinumChest@users.noreply.github.com>
2026-03-31 21:29:11 -0500

    updated site to add work experience, references and a letter of recommendation

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,17 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+	var	 	$window = $(window),
+			$body = $('body'),
+			$nav = $('#nav');
 
 	// Breakpoints.
 		breakpoints({
-			wide:      [ '961px',  '1880px' ],
-			normal:    [ '961px',  '1620px' ],
-			narrow:    [ '961px',  '1320px' ],
-			narrower:  [ '737px',  '960px'  ],
-			mobile:    [ null,     '736px'  ]
+			wide:	 	[ '961px',  '1880px' ],
+			normal:	 	[ '961px',  '1620px' ],
+			narrow:	 	[ '961px',  '1320px' ],
+			narrower:	[ '737px',  '960px'  ],
+			mobile:	 	[ null,	 	'736px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -27,7 +27,9 @@
 		});
 
 	// Nav.
-		var $nav_a = $nav.find('a');
+    
+        // THIS IS THE CORRECTED LINE
+		var $nav_a = $nav.find('a:not(.no-scrolly)');
 
 		$nav_a
 			.addClass('scrolly')
@@ -53,9 +55,9 @@
 			})
 			.each(function() {
 
-				var	$this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
+				var	 	$this = $(this),
+						id = $this.attr('href'),
+						$section = $(id);
 
 				// No section for this link? Bail.
 					if ($section.length < 1)
@@ -119,5 +121,6 @@
 					target: $body,
 					visibleClass: 'header-visible'
 				});
+
 
 })(jQuery);
